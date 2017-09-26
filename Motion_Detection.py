85574bcd6645f2695272dc43c8ff97a05ae44f66
berkott <berkott3002@gmail.com>
2017-09-26 16:47:56 -0400

    fADS
    ADSF

--- Motion_Detection.py
+++ Motion_Detection.py
@@ -6,28 +6,39 @@ def diffImg(t0, t1, t2):
   d2 = cv2.absdiff(t1, t0)
   return cv2.bitwise_and(d1, d2)
 
-cam = cv2.VideoCapture(0)
+def read_img(cam):
+  _, frame=  cam.read()
+  return cv2.GaussianBlur(frame, (5, 5), 0)
 
-winName = "Motin Detection"
+
+cam = cv2.VideoCapture(2)
+
+winName = "main"
 
 # Read three images first:
-t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
+t = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
+t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
 
 while True:
   diff = diffImg(t_minus, t, t_plus)
   number = (np.mean(diff))
   string = str (number)
   cv2.putText(diff, "Mean: " + string, (105, 105), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (225, 0, 0))
-  cv2.imshow( winName, diff)
   print (number)
   font = cv2.FONT_HERSHEY_SIMPLEX
   # Read next image
   t_minus = t
   t = t_plus
-  t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-
+  img = cam.read()[1]
+  t_plus = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+  cv2.imshow("original", t)
+  av_sat = np.mean(hls_img[:,:,2])
+  av_lum = np.mean(hls_img[:,:,1])
+  cv2.putText(diff, "Sat: " + str(av_sat), (105, 160), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (225, 0, 0))
+  cv2.putText(diff, "Lum: " + str(av_lum), (105, 200), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (225, 0, 0))
+  cv2.imshow( winName, diff)
   key = cv2.waitKey(10)
   if key == 27:
     cv2.destroyWindow(winName)
